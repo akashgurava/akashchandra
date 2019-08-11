667fb1269b9c69dcf8e05b78ae1e41c88834b086
Akash <akash.chandra@live.com>
2019-08-11 18:07:11 +0530

    Use gridsome and basic portfolio starter

---
+++ gridsome.client.js
@@ -0,0 +1,11 @@
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { config, library } from '@fortawesome/fontawesome-svg-core'
+import { faGithub, faLinkedin, faTwitter, faFacebook, faDAndD } from '@fortawesome/free-brands-svg-icons'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+
+config.autoAddCss = false;
+library.add(faGithub, faTwitter, faLinkedin, faFacebook)
+
+export default function (Vue) {
+    Vue.component('font-awesome', FontAwesomeIcon)
+}
