deb6e90eeed5994a3e80fb41ea00a9db28262881
Akash <akash.chandra@live.com>
2019-08-19 01:51:05 +0530

    create a hero component

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="content-wrapper bg-background-primary font-sans text-copy-primary leading-normal flex flex-col min-h-screen">
 
-        <!-- Top menu -->
-        <VHeader />
+        <!-- Show the Top menu only if the current path is not homepage -->
+        <VHeader v-if="!isHomePage"/>
 
         <!-- Replaced with child element -->
         <div class="flex-grow">
@@ -20,5 +20,22 @@ export default {
     components: {
         VHeader,
     },
+    data() {
+        return {
+            isHomePage: false
+        }
+    },
+    mounted() {
+        this.homePage()
+    },
+    methods: {
+        homePage() {
+            if(this.$route.path === '/' || this.$route.path.startsWith('/#')) {
+                this.isHomePage = true
+            } else {
+                this.isHomePage = false
+            }
+        }
+    }
 }
 </script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,4 +1,42 @@
 <template>
-  <Layout>
-  </Layout>
+    <Layout>
+        <div class="w-screen h-screen bg-gray-100">
+            <VHeader />
+
+            <div class="hero-content flex flex-col justify-center items-center relative m-0 px-4 py-24 text-center" :style="{'font-family': fonts.logo}">
+                <h1 class="text-2xl lg:text-3xl font-bold">{{ details.name }}</h1>
+                <h2 class="text-xl lg:text-2xl font-bold">{{ details.position }}</h2>
+                <p class="max-w-xs lg:max-w-lg break-normal font-medium py-4 lg:py-6">{{ details.intro }}</p>
+            </div>
+
+        </div>
+    </Layout>
 </template>
+
+<script>
+import { details } from "@/data/user.yaml";
+import { fonts } from "@/data/site.yaml";
+import VHeader from "@/components/VHeader";
+
+export default {
+    components: {
+        VHeader,
+    },
+    data() {
+        return {
+            details: details,
+            fonts: fonts,
+        }
+    }
+}
+</script>
+
+<style scoped>
+.hero-content {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+}
+</style>
